d8139b6c7f2b67dc8c80cd82238b0adcc6875020
Pigmassacre <gulligaolle@gmail.com>
2016-03-23 18:16:24 +0100

    Refactor/cleanup of entitycreator

--- core/src/com/pigmassacre/postwork/managers/EntityCreator.java
+++ core/src/com/pigmassacre/postwork/managers/EntityCreator.java
@@ -16,27 +16,12 @@ import com.pigmassacre.postwork.components.collision.StopMovementOnCollisionComp
 public class EntityCreator {
 
     public static Entity createPlayer(float x, float y, float width, float height) {
-        Entity entity = GameManager.getGame().engine.createEntity();
-        PositionComponent position = GameManager.getGame().engine.createComponent(PositionComponent.class);
-        position.x = x;
-        position.y = y;
-        entity.add(position);
-        VisualComponent visual = GameManager.getGame().engine.createComponent(VisualComponent.class);
-        visual.region = new TextureRegion(new Texture(Gdx.files.internal("badlogic.jpg")), 1, 1);
-        visual.width = width;
-        visual.height = height;
-        entity.add(visual);
-        entity.add(GameManager.getGame().engine.createComponent(PlayerOwnedComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(PlayerControllerComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(JoystickControllerComponent.class));
+        Entity entity = createBasicMovingEntity(x, y, width, height);
+
+        makePlayerControlled(entity);
+        makeVisual(entity, width, height);
+
         entity.add(GameManager.getGame().engine.createComponent(ShootingComponent.class));
-        CollisionComponent collision = GameManager.getGame().engine.createComponent(CollisionComponent.class);
-        collision.init(width, height);
-        entity.add(collision);
-        entity.add(GameManager.getGame().engine.createComponent(StopMovementOnCollisionComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(AccelerationComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(DragComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(VelocityComponent.class));
         entity.add(GameManager.getGame().engine.createComponent(CameraFocusComponent.class));
 
         GameManager.getGame().engine.addEntity(entity);
@@ -45,29 +30,10 @@ public class EntityCreator {
     }
 
     public static Entity createHomingEnemy(float x, float y, float width, float height, Entity homingTarget) {
-        Entity entity = GameManager.getGame().engine.createEntity();
-        PositionComponent position = GameManager.getGame().engine.createComponent(PositionComponent.class);
-        position.x = x;
-        position.y = y;
-        entity.add(position);
-        VisualComponent visual = GameManager.getGame().engine.createComponent(VisualComponent.class);
-        visual.region = new TextureRegion(new Texture(Gdx.files.internal("badlogic.jpg")), 1, 1);
-        visual.width = width;
-        visual.height = height;
-        entity.add(visual);
-        CollisionComponent collision = GameManager.getGame().engine.createComponent(CollisionComponent.class);
-        collision.init(width, height);
-        entity.add(collision);
-        entity.add(GameManager.getGame().engine.createComponent(StopMovementOnCollisionComponent.class));
-        //entity.add(GameManager.getGame().engine.createComponent(DamageOnCollisionComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(AccelerationComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(DragComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(VelocityComponent.class));
+        Entity entity = createBasicMovingEntity(x, y, width, height);
 
-        HomingComponent homing = GameManager.getGame().engine.createComponent(HomingComponent.class);
-        homing.target = homingTarget;
-        entity.add(homing);
-        entity.add(GameManager.getGame().engine.createComponent(AngleComponent.class));
+        makeVisual(entity, width, height);
+        makeHoming(entity, homingTarget);
 
         GameManager.getGame().engine.addEntity(entity);
 
@@ -75,31 +41,58 @@ public class EntityCreator {
     }
 
     public static Entity createBullet(float x, float y, float width, float height, Entity homingTarget) {
+        Entity entity = createBasicMovingEntity(x, y, width, height);
+
+        makeVisual(entity, width, height);
+        makeHoming(entity, homingTarget);
+
+        entity.add(GameManager.getGame().engine.createComponent(DamageOnCollisionComponent.class));
+
+        GameManager.getGame().engine.addEntity(entity);
+
+        return entity;
+    }
+
+    public static Entity createBasicMovingEntity(float x, float y, float width, float height) {
         Entity entity = GameManager.getGame().engine.createEntity();
         PositionComponent position = GameManager.getGame().engine.createComponent(PositionComponent.class);
         position.x = x;
         position.y = y;
         entity.add(position);
+        CollisionComponent collision = GameManager.getGame().engine.createComponent(CollisionComponent.class);
+        collision.init(width, height);
+        entity.add(collision);
+        entity.add(GameManager.getGame().engine.createComponent(AccelerationComponent.class));
+        entity.add(GameManager.getGame().engine.createComponent(DragComponent.class));
+        entity.add(GameManager.getGame().engine.createComponent(VelocityComponent.class));
+        entity.add(GameManager.getGame().engine.createComponent(StopMovementOnCollisionComponent.class));
+
+        return entity;
+    }
+
+    public static Entity makePlayerControlled(Entity entity) {
+        entity.add(GameManager.getGame().engine.createComponent(PlayerOwnedComponent.class));
+        entity.add(GameManager.getGame().engine.createComponent(PlayerControllerComponent.class));
+        entity.add(GameManager.getGame().engine.createComponent(JoystickControllerComponent.class));
+
+        return entity;
+    }
+
+    public static Entity makeVisual(Entity entity, float width, float height) {
         VisualComponent visual = GameManager.getGame().engine.createComponent(VisualComponent.class);
         visual.region = new TextureRegion(new Texture(Gdx.files.internal("badlogic.jpg")), 1, 1);
         visual.width = width;
         visual.height = height;
         entity.add(visual);
-        CollisionComponent collision = GameManager.getGame().engine.createComponent(CollisionComponent.class);
-        collision.init(width, height);
-        entity.add(collision);
-        entity.add(GameManager.getGame().engine.createComponent(StopMovementOnCollisionComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(DamageOnCollisionComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(AccelerationComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(DragComponent.class));
-        entity.add(GameManager.getGame().engine.createComponent(VelocityComponent.class));
 
+        return entity;
+    }
+
+    public static Entity makeHoming(Entity entity, Entity homingTarget) {
         HomingComponent homing = GameManager.getGame().engine.createComponent(HomingComponent.class);
         homing.target = homingTarget;
         entity.add(homing);
         entity.add(GameManager.getGame().engine.createComponent(AngleComponent.class));
-
-        GameManager.getGame().engine.addEntity(entity);
 
         return entity;
     }
